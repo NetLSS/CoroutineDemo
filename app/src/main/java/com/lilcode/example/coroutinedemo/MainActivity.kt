8d6b09edd7a42689bba436cc4a97bcdb27856641
NetLSS <futureprogrammer@naver.com>
2021-08-26 11:24:20 +0900

    Feat: SeekBar 리스너 구현

--- app/src/main/java/com/lilcode/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/coroutinedemo/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.lilcode.example.coroutinedemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import com.lilcode.example.coroutinedemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
 
+    private var count: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            @SuppressLint("SetTextI18n")
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                count = progress
+                binding.countText.text = "$count coroutines"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
     }
 }
