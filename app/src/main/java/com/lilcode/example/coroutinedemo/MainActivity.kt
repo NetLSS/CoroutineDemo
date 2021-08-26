ee72c12b279e170a427e26c5851465140278ff76
NetLSS <futureprogrammer@naver.com>
2021-08-26 12:07:21 +0900

    Feat: 코루틴 시작 시키고 결과 받기

--- app/src/main/java/com/lilcode/example/coroutinedemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/coroutinedemo/MainActivity.kt
@@ -3,14 +3,18 @@ package com.lilcode.example.coroutinedemo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.SeekBar
 import com.lilcode.example.coroutinedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
 
     private var count: Int = 1
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +35,20 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    suspend fun performTask(taskNumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(7_777)
+            return@async "Finished Coroutine $taskNumber"
+        }
+
+    fun launchCoroutines(view: View) {
+        (1..count).forEach {
+            binding.statusText.text = "Started Coroutine $it"
+            coroutineScope.launch(Dispatchers.Main) {
+                binding.statusText.text = performTask(it).await()
+            }
+        }
     }
 }
